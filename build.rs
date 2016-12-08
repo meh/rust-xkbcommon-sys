ca6eef4499ed54d5dd5bc49d9d1216364715d3b1
meh <meh@schizofreni.co>
2016-12-08 04:54:00 +0100

    Initial commit

---
+++ build.rs
@@ -0,0 +1,45 @@
+use std::env;
+
+extern crate pkg_config;
+use pkg_config::{Config, Error};
+
+fn is_static() -> bool {
+	env::var("CARGO_FEATURE_STATIC").is_ok()
+}
+
+fn common() -> Result<(), Error> {
+	if let Ok(path) = env::var("XKBCOMMON_LIB_DIR") {
+		for lib in &["xkbcommon"] {
+			println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
+		}
+
+		println!("cargo:rustc-link-search=native={}", path);
+	}
+	else {
+		try!(Config::new().statik(is_static()).probe("xkbcommon"));
+	}
+
+	Ok(())
+}
+
+fn x11() -> Result<(), Error> {
+	if env::var("CARGO_FEATURE_X11").is_ok() {
+		if let Ok(path) = env::var("XKBCOMMON_LIB_DIR") {
+			for lib in &["xkbcommon-x11"] {
+				println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
+			}
+
+			println!("cargo:rustc-link-search=native={}", path);
+		}
+		else {
+			try!(Config::new().statik(is_static()).probe("xkbcommon-x11"));
+		}
+	}
+
+	Ok(())
+}
+
+fn main() {
+	common().unwrap();
+	x11().unwrap();
+}
---
+++ src/names.rs
@@ -0,0 +1,25 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyleft (ↄ) meh. <meh@schizofreni.co> | http://meh.schizofreni.co
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+pub const XKB_MOD_NAME_SHIFT: &'static [u8] = b"Shift\0";
+pub const XKB_MOD_NAME_CAPS:  &'static [u8] = b"Lock\0";
+pub const XKB_MOD_NAME_CTRL:  &'static [u8] = b"Control\0";
+pub const XKB_MOD_NAME_ALT:   &'static [u8] = b"Mod1\0";
+pub const XKB_MOD_NAME_NUM:   &'static [u8] = b"Mod2\0";
+pub const XKB_MOD_NAME_LOGO:  &'static [u8] = b"Mod4\0";
+
+pub const XKB_LED_NAME_CAPS:   &'static [u8] = b"Caps Lock\0";
+pub const XKB_LED_NAME_NUM:    &'static [u8] = b"Num Lock\0";
+pub const XKB_LED_NAME_SCROLL: &'static [u8] = b"Scroll Lock\0";
+
