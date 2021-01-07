1838918630b0de09224317a0e6fa3faf28ddb1cb
Rafal Michalski <royaltm75@gmail.com>
2021-01-07 04:46:44 +0100

    build.sh: fix dynlib -> dylib

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ fn is_static() -> bool {
 fn common() -> Result<(), Error> {
 	if let Ok(path) = env::var("XKBCOMMON_LIB_DIR") {
 		for lib in &["xkbcommon"] {
-			println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
+			println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dylib" }, lib);
 		}
 
 		println!("cargo:rustc-link-search=native={}", path);
@@ -26,7 +26,7 @@ fn x11() -> Result<(), Error> {
 	if env::var("CARGO_FEATURE_X11").is_ok() {
 		if let Ok(path) = env::var("XKBCOMMON_LIB_DIR") {
 			for lib in &["xkbcommon-x11"] {
-				println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dynlib" }, lib);
+				println!("cargo:rustc-link-lib={}={}", if is_static() { "static" } else { "dylib" }, lib);
 			}
 
 			println!("cargo:rustc-link-search=native={}", path);
